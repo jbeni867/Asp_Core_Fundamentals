4cfc913c7d6a8eb52245a5d61af2744f4c0f2076
jbeni867 <jordybeni867@gmail.com>
2024-06-25 19:58:36 -0500

    Syncing changes -- info in next commit

--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -20,5 +20,13 @@ namespace PieShop.Controllers
             PieListViewModel pieListViewModel = new(_pieRepository.AllPies, "Cheese cakes");
             return View(pieListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
--- PieShop/obj/Debug/net8.0/PieShop.AssemblyInfo.cs
+++ PieShop/obj/Debug/net8.0/PieShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ff7f3941e06cdcc3f231c27604d17a53c2379c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
